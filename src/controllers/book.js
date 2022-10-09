45b9fe650f5316544a484122d977feec58acce73
hip06 <khongpikten21@gmail.com>
2022-10-09 10:23:51 +0700

    v16

--- src/controllers/book.js
+++ src/controllers/book.js
@@ -1,6 +1,6 @@
 import * as services from "../services"
 import { interalServerError, badRequest } from "../middlewares/handle_errors"
-import { title, image, category_code, price, available, bid, bids, filename } from "../helpers/joi_schema"
+import { title, image, category_code, price, available, bid, bids, filename, description } from "../helpers/joi_schema"
 import joi from 'joi'
 const cloudinary = require('cloudinary').v2;
 
@@ -17,7 +17,7 @@ export const getBooks = async (req, res) => {
 export const createNewBook = async (req, res) => {
     try {
         const fileData = req.file
-        const { error } = joi.object({ title, image, category_code, price, available }).validate({ ...req.body, image: fileData?.path })
+        const { error } = joi.object({ title, image, category_code, price, available, description }).validate({ ...req.body, image: fileData?.path })
         if (error) {
             if (fileData) cloudinary.uploader.destroy(fileData.filename)
             return badRequest(error.details[0].message, res)
